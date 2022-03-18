d5fe08392fd4e845f0b920075035018123522e6e
ipozdeev <igor.posdeev@gmail.com>
2022-03-18 16:50:48 +0100

    add visuals, more recipes, first estimates

--- src/recipes.py
+++ src/recipes.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -5,30 +6,37 @@ from multiprocessing import Pool, cpu_count
 from scipy.integrate import simps
 from functools import reduce
 from tensorflow_probability import distributions as tfd
+from joblib import Memory
+from itertools import product
 
 from optools.blackscholes import option_price as bs_price
 from optools.volsmile import VolatilitySmile
 from optools.rnd import fit_lognormal_mix
 
 
-def mf_prob_estimator(df_: pd.DataFrame):
-    """Estimate rn prob of S>85.
+memory = Memory(location=os.path.join(os.environ.get("PROJECT_ROOT"), "data"),
+                verbose=False)
+
+
+def mf_prob_estimator(df_: pd.DataFrame, thresh=85.0) -> float:
+    """Estimate rn prob of S>s.
 
     Parameters
     ----------
     df_
         from pd.groupby; with columns 'forward', 'div_yield' etc.
-
-    Returns
-    -------
-
+    thresh : float
+        the value of s in P[S>s]
     """
-    x = np.arange(85, 150, 1e-04)
+    x = np.arange(thresh, 200, 1e-04)
+
+    # forward rate and interest rates
     f_ = df_["forward"].iloc[0]
     r_ = df_[["rf", "div_yield"]].iloc[0]
 
+    # construct smile
     vs = VolatilitySmile(
-        vol_series=df_[["strike", "vol"]].set_index("strike").squeeze(),
+        vol_series=df_[["strike", "vola"]].set_index("strike").squeeze(),
         tau=1/12
     )
 
@@ -41,16 +49,43 @@ def mf_prob_estimator(df_: pd.DataFrame):
     return res
 
 
+def mf_var_estimator(df_: pd.DataFrame) -> float:
+    """Estimate model-free variance.
+
+    Parameters
+    ----------
+    df_
+        from pd.groupby; with columns 'forward', 'rf' etc.
+    """
+    # x = np.arange(85, 200, 1e-04)
+    f_ = df_["forward"].iloc[0]
+    r_ = df_[["rf", "div_yield"]].iloc[0]
+
+    vs = VolatilitySmile(
+        vol_series=df_[["strike", "vola"]].set_index("strike").squeeze(),
+        tau=1/12
+    )
+
+    # differentiate w.r.t. K to ged the rnd
+    res = vs\
+        .interpolate(kind="cubic",
+                     new_x=np.arange(60, max(100, f_*1.3), 1e-04),
+                     extrapolate=True)\
+        .get_mfivariance(forward=f_, rf=r_["rf"], svix=True)
+
+    return res
+
+
 def lnmix_prob_estimator(df_: pd.DataFrame):
     """
     """
     f_ = df_["forward"].iloc[0]
     r_ = df_[["rf", "div_yield"]].iloc[0]
-    k_, v_ = df_["strike"].values, df_["vol"].values
+    k_, v_ = df_["strike"].values, df_["vola"].values
 
     # compute option prices first
     c_price = bs_price(
-        strike=k_, vol=v_, forward=f_,
+        strike=k_, vola=v_, forward=f_,
         rf=r_["rf"], div_yield=r_["div_yield"],
         is_call=True, tau=1/12
     )
@@ -82,10 +117,11 @@ def lnmix_prob_estimator(df_: pd.DataFrame):
     return res
 
 
+@memory.cache(ignore=["parallelize"])
 def estimate_probability(strikes_vola, forward, rates,
                          kind="parametric", parallelize=False) \
         -> pd.Series:
-    """Estimate parameters of the mixture of 2 log-normals.
+    """Estimate probability of invasion in the run-up to it.
 
     Assumes maturity of options and forwards to be 1/12.
 
@@ -129,6 +165,93 @@ def estimate_probability(strikes_vola, forward, rates,
         res = dict()
         for dt, dt_grp in tqdm(data.groupby("date")):
             res[dt] = func_to_apply(dt_grp)
+        res = pd.Series(res)
+
+    return res
+
+
+@memory.cache
+def estimate_probability_multiple_levels(strikes_vola, forward, rates) \
+        -> pd.DataFrame:
+    """Estimate probability of invasion in the run-up to it.
+
+    Assumes maturity of options and forwards to be 1/12.
+
+    Parameters
+    ----------
+    strikes_vola
+    forward
+    rates
+        in frac of 1 p.a.
+    """
+    # drop dates with <5 observations
+    n_per_date = strikes_vola.groupby("date").count().iloc[:, 0]
+    good_dt = n_per_date.loc[n_per_date.gt(4)].index
+
+    # merge all the necessary data (produces duplicates for some vars)
+    data = reduce(
+        lambda x_, y_: pd.merge(x_, y_, on="date", how="inner"),
+        [strikes_vola, forward, rates]
+    )
+    data = data.loc[data["date"].isin(good_dt)].sort_values("date")
+
+    #
+    itr = product([grp_ for _, grp_ in data.groupby("date")],
+                  range(85, 136, 10))
+
+    with Pool(cpu_count()) as p:
+        ret_list = p.starmap(mf_prob_estimator, itr)
+
+    res = pd.Series(
+        ret_list,
+        index=pd.MultiIndex.from_product(
+            [[n_ for n_, _ in data.groupby("date")], range(85, 136, 10)],
+            names=["date", "thresh"])
+    ).unstack()
+
+    return res
+
+
+@memory.cache(ignore=["parallelize"])
+def estimate_mfiv(strikes_vola, forward, rates, parallelize=False) \
+        -> pd.Series:
+    """Estimate MFIV.
+
+    Assumes maturity of options and forwards to be 1/12.
+
+    Parameters
+    ----------
+    strikes_vola
+    forward
+    rates
+        in frac of 1 p.a.
+    parallelize : bool
+        True to parallelize
+    """
+    # drop dates with <5 observations
+    n_per_date = strikes_vola.groupby("date").count().iloc[:, 0]
+    good_dt = n_per_date.loc[n_per_date.gt(4)].index
+
+    # merge all the necessary data (produces duplicates for some vars)
+    data = reduce(
+        lambda x_, y_: pd.merge(x_, y_, on="date", how="inner"),
+        [strikes_vola, forward, rates]
+    )
+    data = data.loc[data["date"].isin(good_dt)].sort_values("date")
+
+    # function to apply in parallel
+    def apply_parallel(grp, func):
+        with Pool(cpu_count()) as p:
+            ret_list = p.map(func, [group for _, group in grp])
+        res_ = pd.Series(ret_list, index=[n_ for n_, _ in grp])
+        return res_
+
+    if parallelize:
+        res = apply_parallel(data.groupby("date"), mf_var_estimator)
+    else:
+        res = dict()
+        for dt, dt_grp in tqdm(data.groupby("date")):
+            res[dt] = mf_var_estimator(dt_grp)
         res = pd.Series(res)
 
     return res
--- src/visuals.py
+++ src/visuals.py
@@ -10,6 +10,39 @@ from .datafeed_.downstream import get_timeline
 palette = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 
+def plot_spot(spot: pd.Series) -> plt.Figure:
+    """Plot spot exchange rate."""
+    to_plot = spot \
+        .resample("1T").asfreq() \
+        .rename("usdrub")
+    dt_t = get_timeline().index[0]
+
+    # canvas
+    fig, ax = plt.subplots(1, 2, sharey=True, figsize=(8, 4))
+
+    # labels
+    ax[0].set_title("feb 15th - 28th")
+    ax[0].set_ylabel("usdrub")
+    ax[1].set_title("feb 24th")
+
+    # lines
+    to_plot.loc["2022-02-15":].plot(ax=ax[0])
+    to_plot.loc["2022-02-23 18:00":"2022-02-24"].plot(ax=ax[1])
+    for ax_ in ax:
+        ax_.axvline(x=dt_t, color=palette[2], label="announcement", alpha=0.5)
+
+    # legend
+    leg_handles = [
+        mlines.Line2D([], [], color=palette[0], label="spot rate"),
+        mlines.Line2D([], [], color=palette[2], label="announcement")
+    ]
+    ax[1].legend(handles=leg_handles)
+
+    fig.tight_layout()
+
+    return fig
+
+
 def plot_rates(rates: pd.DataFrame) -> plt.Figure:
     """Plot interest rates.
 
@@ -18,13 +51,15 @@ def plot_rates(rates: pd.DataFrame) -> plt.Figure:
     rates : pd.DataFrame
         indexed by datetime, with columns 'rf' and 'div_yield'
     """
+    to_plot = rates.resample("1T").asfreq()
+
     # canvas
     fig, ax_l = plt.subplots()
     ax_r = ax_l.twinx()
 
     # lines
-    rates["rf"].plot(ax=ax_l, color=palette[0])
-    rates["div_yield"].plot(ax=ax_r, color=palette[2])
+    to_plot["rf"].plot(ax=ax_l, color=palette[0])
+    to_plot["div_yield"].plot(ax=ax_r, color=palette[2])
 
     # legend
     leg_handles = [
@@ -46,16 +81,19 @@ def plot_rates(rates: pd.DataFrame) -> plt.Figure:
     return fig
 
 
-def plot_invasion_probability(prob: pd.Series) -> plt.Figure:
+def plot_invasion_probability(prob: pd.Series, show_invasion: bool = False) \
+        -> plt.Figure:
     """Plot prob of invasion from 01/01/2022 up to 02/24."""
-    dt_t = get_timeline().index[0]
 
     # canvas
     fig, ax = plt.subplots(figsize=(8, 4))
 
     # lines
     prob.plot(ax=ax, linestyle="none", marker=".")
-    ax.axvline(x=dt_t, color=palette[2], label="announcement", alpha=0.5)
+
+    if show_invasion:
+        dt_t = get_timeline().index[0]
+        ax.axvline(x=dt_t, color=palette[2], label="announcement", alpha=0.5)
 
     # labels
     ax.set_ylabel(r"$P[S > s]$")
@@ -64,8 +102,11 @@ def plot_invasion_probability(prob: pd.Series) -> plt.Figure:
     # legend
     leg_handles = [
         mlines.Line2D([], [], color=palette[0], label=r"$P[S>s]$"),
-        mlines.Line2D([], [], color=palette[2], label="announcement")
     ]
+    if show_invasion:
+        leg_handles += [
+            mlines.Line2D([], [], color=palette[2], label="announcement")
+        ]
     ax.legend(handles=leg_handles)
 
     # ticks
@@ -86,13 +127,13 @@ def plot_invasion_probability_zoomed(prob: pd.Series) -> plt.Figure:
     fig, ax = plt.subplots(figsize=(8, 4))
 
     # lines
-    prob.loc["2022-02-23":"2022-02-24"]\
+    prob.loc["2022-02-23":].loc[:dt_t]\
         .plot(ax=ax, linestyle="none", marker=".")
     ax.axvline(x=dt_t, color=palette[2], label="announcement", alpha=0.5)
 
     # ticks
     ax.set_xticklabels(ax.get_xticklabels(), rotation=0, ha="center")
-    ax.set_yticks(np.arange(0.2, 0.4, 0.05))
+    # ax.set_yticks(np.arange(0.2, 0.4, 0.05))
 
     # labels
     ax.set_ylabel(r"$P[S > s]$")
